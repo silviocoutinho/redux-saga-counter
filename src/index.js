d100d59cb7d7496dfcee64b136e473c245d6c8d7
Silvio Ferreira Coutinho <silviofc79@gmail.com>
2022-04-21 20:40:04 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import Counter from './components/Counter'
+import reducer from './reducers'
+import rootSaga from './sagas'
+
+// https://github.com/redux-saga/redux-saga/tree/master/examples/counter
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSaga)
+
+const action = type => store.dispatch({ type })
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementIfOdd={() => action('INCREMENT_IF_ODD')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
